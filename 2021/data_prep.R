57eb97e1ddffbe8fb4647d99f73ad017e8fa97c2
Eric Yount <eoyount@gmail.com>
2021-01-20 14:00:52 -0500

    Archived old files and began 2021 predictions

---
+++ 2021/data_prep.R
@@ -0,0 +1,5 @@
+# I intend to modify https://jlaw.netlify.app/2021/01/07/predicting-the-winner-of-super-bowl-lv/
+# to work for the NCAA tournament
+
+# devtools::install_github("jflancer/bigballR")
+library(bigballR)
